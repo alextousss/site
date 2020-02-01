dbe2b2fb769ccbaf185df8a1338f04c66921f84d
alex <toussaintalex@free.fr>
2020-02-01 18:23:09 +0100

    maintenant avec PYTHON

--- build.py
+++ build.py
@@ -5,31 +5,43 @@ import markdown
 file_loader = FileSystemLoader(".")
 env = Environment(loader=file_loader)
 
+os.system("rm -r build/*")
+os.system("cp -r images build/images")
+
+# On fait les pages normales
+for filename in os.listdir("."):
+    if filename.endswith(".html"):
+        with open("build/" + filename, "w") as f:
+            f.write(env.get_template(filename).render())
+            print("Built " + filename)
+
 
 # On fait la liste des posts
 posts = []
 for filename in os.listdir("posts"):
-    index = filename.split(".")[0]
-    subject = filename.split(".")[1].replace("-", " ")
-    posts.append((subject, filename))
+    date = filename.split("_")[0]
+    subject = filename.split("_")[1].split(".")[0].replace("-", " ")
+    posts.append(
+        {
+            "date": date,
+            "subject": subject,
+            "url": filename.replace("md", "html"),
+            "filename": filename,
+        }
+    )
+print(posts)
 
 # On fait la page d'acceuil des posts
 with open("build/thoughts.html", "w") as f:
-    f.write(env.get_template("thoughts.html").render(posts=posts))
+    string = env.get_template("thoughts.html").render(posts=posts)
+    f.write(string)
     print("Built thoughts.html")
 
 
 # On build chaque post
 for post in posts:
-    with open("build/" + post[1].replace("md", "html"), "w") as f:
-        with open("posts/" + post[1], "r") as f_source:
+    with open("build/" + post["filename"].replace("md", "html"), "w") as f:
+        with open("posts/" + post["filename"], "r") as f_source:
             html = markdown.markdown(f_source.read())
             f.write(env.get_template("post-base.html").render(post=html))
-        print("Built thoughts.html")
-
-
-# On fait les pages normales
-for filename in os.listdir("."):
-    with open("build/" + filename, "w") as f:
-        f.write(env.get_template("thoughts.html").render())
-        print("Built thoughts.html")
+        print("Built " + post["filename"])
